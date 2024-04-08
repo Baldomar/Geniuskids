0406089888d7f538d0ba9b1c9dd1e6f11ffecf19
Baldomar <jeisonventy10@gmail.com>
2024-04-07 18:31:20 -0600

    Finalizado

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,9 @@
+<!------------- Barra de Navegacion ---------------------->
+<nav>
+    <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/nosotros">Acerca de Nosotros</a></li>
+        <li class="nombre_centro"><a>GeniusKids</a></li>
+    </ul>
+</nav>
+<!-- ------------------------------------------------- -->
